37ddaaecfd4d5125d395c5e8f226f82e7541e61a
Minseok Lee <55467050+mslee300@users.noreply.github.com>
2024-08-17 14:03:23 +0000

    switch to model gpt-4o

--- django_project/views.py
+++ django_project/views.py
@@ -6,31 +6,35 @@ from .forms import AnswerForm
 
 openai.api_key = os.environ['OPENAI']
 
+
 # AI message generator
 def generate_message(request, prompt):
     response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    temperature=0,
-    max_tokens=150,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
-    messages=[
-          {"role": "user", "content": f"Give me a Biblical answer to this question with a Bible reference: {prompt}"}
-      ]
-    )
+        model="gpt-4o",
+        temperature=0,
+        max_tokens=600,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        messages=[{
+            "role":
+            "user",
+            "content":
+            f"Give me a Biblical answer to this question with a Bible reference: {prompt}"
+        }])
     return response['choices'][0]['message']['content']
+
 
 # Main page
 def index(request):
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
-            question= form['prompt'].value()
+            question = form['prompt'].value()
             message = generate_message(request, form['prompt'].value())
             context = {
-              'question': question,
-              'message': message,
+                'question': question,
+                'message': message,
             }
         return render(request, 'answer.html', context)
     else:
